34ca32096c2ebef9277aa286d1e4eba14e3c2241
nyedidikeke <me@nyedidikeke.com>
2020-01-25 06:17:09 +0000

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components"
+
+const NotFoundPage = () => {
+    return (
+        <Layout>
+            <div style={{ textAlign: `center` }}>Page not found (404 error)</div>
+        </Layout>
+    )
+}
+
+export default NotFoundPage
