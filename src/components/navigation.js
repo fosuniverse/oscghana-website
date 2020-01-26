e667bbfe1325968071d4b85e7159b4fd98ed9a59
nyedidikeke <me@nyedidikeke.com>
2020-01-26 17:15:14 +0000

    Update navigation.js
    - Minor import stylesheet improvement.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Logo from "./logo"
-import style from "../assets/css/index.css"
+import "../assets/css/index.css"
 
 export default class Navigation extends React.Component {
   constructor(props) {
