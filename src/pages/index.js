1c6fad97c9bf9f70ab95c721a776aa629acaacb3
nyedidikeke <me@nyedidikeke.com>
2020-02-01 17:03:30 +0000

    Add homepage hero section to index.js
    - Add _header.scss partial.
    - Refactor _index.scss partial.
    - Add hero section to index.js as "header" parameter.
    - Update stylesheets.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,34 @@
 import React from "react"
 
 import Layout from "../components/index"
+import oscGhanaMap from "../images/brand/osc_ghana_map.svg"
 
 const IndexPage = () => (
   <Layout
+    header={
+      <section className={"hero tw-clearfix"}>
+        <div className={"container tw-flex tw-flex-wrap"}>
+          <div className={"hero__caption tw-w-full md:tw-w-1/2"}>
+            <div className={"hero__description"}>
+              <strong className={"hero__strong"}>A community</strong> of
+              passionate technology enthusiasts who dedicate work towards{" "}
+              <strong className={"hero__strong"}>
+                building, enhancing and adding value
+              </strong>{" "}
+              to <strong className={"hero__strong"}>open source tools</strong>{" "}
+              in Ghana.
+            </div>
+          </div>
+          <div className={"hero__banner tw-w-full md:tw-w-1/2"}>
+            <img
+              className={"hero__img"}
+              src={oscGhanaMap}
+              alt={"OSC Ghana Map"}
+            />
+          </div>
+        </div>
+      </section>
+    }
     main={
       <div>
         {/* A demo display text, delete before actual development */}
