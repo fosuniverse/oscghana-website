66f58f4a21aedb7b53a9d1ce712078075211f207
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-02-08 18:55:03 +0000

    add Ghana map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Ghana from "../images/home/ghana.svg"
 
 const IndexPage = () => {
   return (
     <div>
       {/* A demo display text, delete before actual development */}
       <center>WELCOME TO OPEN SOURCE COMMUNITY GHANA</center>
+      <img src={Ghana} alt="" />
     </div>
   )
 }
